11fe0c469577a6a56970d0fa74c213b9135ba6e3
BillChee123 <colo316@hotmail.com>
2021-07-18 16:51:08 +0800

    Add scripts

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+- For VM installation purposes
+- Only use this repository if you know what you are doing
---
+++ lxcfs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo rm -rf /var/lib/lxcfs 
+sudo mkdir -p /var/lib/lxcfs
+sudo lxcfs /var/lib/lxcfs
+
